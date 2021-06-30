c154a5f55d1a9af66f5c55fbcaa61de5e52d4c46
Iyanda Oluwasegun <iyandaoluwasegun7@gmail.com>
2021-06-30 19:22:25 +0100

    eslint edit

--- middlewares/authVerification.js
+++ middlewares/authVerification.js
@@ -7,23 +7,23 @@ let router = express.Router();
 
 export const validateToken = async (req, res, next) => {
     
-    try {
-        const authorizationHeader = req.headers.authorization;
-        let result;
-        if (authorizationHeader) {
-            const token = authorizationHeader.split(' ')[1];
-            result = await jwt.verify(token, process.env.JWT_SECRET_KEY)
-            req.user = result;
-            next();
-        } else {
-            result = {
-                error: `Authentication error. Token missing`, status: 401
-            };
-            res.status(401).json({ result });
-        }
-    } catch (error) {
-        res.status(401).json({message: "Token expired"})
-    };
+	try {
+		const authorizationHeader = req.headers.authorization;
+		let result;
+		if (authorizationHeader) {
+			const token = authorizationHeader.split(" ")[1];
+			result = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+			req.user = result;
+			next();
+		} else {
+			result = {
+				error: "Authentication error. Token missing", status: 401
+			};
+			res.status(401).json({ result });
+		}
+	} catch (error) {
+		res.status(401).json({message: "Token expired"});
+	}
 };
 
 export default router;
